47b5f0c3bf27fd7250a52c96c89f34f4bde1ee6f
UshakovaNataliia <65922563+UshakovaNataliia@users.noreply.github.com>
2020-10-26 23:34:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,54 +1,73 @@
 const refs = {
     daysFace: document.querySelector('span[data-value="days"]'),
-   hoursFace: document.querySelector('span[data-value="hours"]'),
+    hoursFace: document.querySelector('span[data-value="hours"]'),
     minsFace: document.querySelector('span[data-value="mins"]'),
     secsFace: document.querySelector('span[data-value="secs"]'),
-  };
+};
 
-  class CountdownTimer {
-    constructor({ selector, targetDate }) {
-      this.selector = selector;
-      this.targetDate = targetDate;
-      this.intervalId = null;
-      this.start();
-    };
+//! ---------Решение обычными ф-циями--------- 
+// setInterval(() => {
+//    const startTime = new Date('Nov 15, 2020');
+//    const currentTime = Date.now();
+//    const deltaTime = startTime - currentTime;
 
-    start() {
+//    updateClockFace(deltaTime);
+// }, 1000);
+
+// function updateClockFace (time) {  
+//    const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+//    const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+//    const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+//    const secs = pad(Math.floor((time % (1000 * 60)) / (1000)));
+ 
+//    refs.daysFace.textContent = days;
+//    refs.hoursFace.textContent = hours;
+//    refs.minsFace.textContent = mins;
+//    refs.secsFace.textContent = secs;
+// };
+
+// function pad(value) {
+//     return String(value).padStart(2, '0');
+// };
+
+
+//! ------------Решение через класс------------
+class CountdownTimer{
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+    this.start();
+  }
+
+  start(){
+    setInterval(() => {
     const startTime = this.targetDate;
     const currentTime = Date.now();
     const deltaTime = startTime - currentTime;
 
     this.updateClockFace(deltaTime);
-    
-    this.intervalId = setInterval(() => {
-      const currentTime = Date.now();
-      const deltaTime = startTime - currentTime;
+  }, 1000);
+  }
 
-      this.updateClockFace(deltaTime);
-    }, 1000);
-    };
-
-    updateClockFace (time) {  
-     const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+  updateClockFace (time) {  
+    const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
     const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-     const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-     const secs = pad(Math.floor((time % (1000 * 60)) / (1000)));
-    
+    const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = pad(Math.floor((time % (1000 * 60)) / (1000)));
+
     refs.daysFace.textContent = days;
     refs.hoursFace.textContent = hours;
     refs.minsFace.textContent = mins;
     refs.secsFace.textContent = secs;
-    };
+  }
 };
 
 function pad(value) {
-    return String(value).padStart(2, '0');
-  };
-
+  return String(value).padStart(2, '0');
+};
 
 const timer = new CountdownTimer({
-    selector: '#timer-1',
-    targetDate: new Date('Aug 24, 2020'),
+  selector: '#timer-1',
+  targetDate: new Date('Nov 15, 2020'),
 });
-
 
